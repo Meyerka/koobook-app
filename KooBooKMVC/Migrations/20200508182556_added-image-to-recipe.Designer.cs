234958d201cc2828a0e9d14a66f47015a7754691
Karl-Erik Meyer <karlerik.meyer@protime.eu>
2020-05-09 20:36:41 +0200

    Session features

--- KooBooKMVC/Migrations/20200508182556_added-image-to-recipe.Designer.cs
+++ KooBooKMVC/Migrations/20200508182556_added-image-to-recipe.Designer.cs
@@ -4,14 +4,16 @@ using KooBooKMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KooBooKMVC.Migrations
 {
     [DbContext(typeof(KoobookDbContext))]
-    partial class KoobookDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200508182556_added-image-to-recipe")]
+    partial class addedimagetorecipe
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ KooBooKMVC/Migrations/20200508182556_added-image-to-recipe.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KooBooKMVC.Migrations
+{
+    public partial class addedimagetorecipe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Recipes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Recipes");
+        }
+    }
+}
