2d9d8e2da7a9cae2c244d9e72587939f9112d8cb
Karl-Erik Meyer <karlerik.meyer@protime.eu>
2020-05-10 16:31:07 +0200

    GroceryList first features (Add/Delete features)

---
+++ KooBooKMVC/Areas/Common/Controllers/GroceryListController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KooBooKMVC.Extensions;
+using KooBooKMVC.Models;
+using KooBooKMVC.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KooBooKMVC.Areas.Common.Controllers
+{
+    [Area("Common")]
+    public class GroceryListController : Controller
+    {
+        [BindProperty]
+        public GroceryListViewModel GroceryList { get; set; }
+        private readonly IIngredientData _ingredientData;
+
+        public GroceryListController(IIngredientData ingredientData)
+        {
+            _ingredientData = ingredientData;
+            GroceryList = new GroceryListViewModel
+            {
+                IngredientList = new List<Ingredient>()
+            };
+        }
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetObject<List<int>>("GroceryList") != null)
+            {
+                List<int> sessionList = new List<int>();
+                sessionList = HttpContext.Session.GetObject<List<int>>("GroceryList");
+                foreach (var id in sessionList)
+                {
+                    GroceryList.IngredientList.Add(_ingredientData.GetById(id));
+                }
+            }
+            return View(GroceryList);
+        }
+
+        public IActionResult DeleteItem(int ingredientId)
+        {
+            if (HttpContext.Session.GetObject<List<int>>("GroceryList") != null)
+            {
+                List<int> sessionList = new List<int>();
+                sessionList = HttpContext.Session.GetObject<List<int>>("GroceryList");
+                sessionList.Remove(ingredientId);
+
+                HttpContext.Session.SetObject("GroceryList", sessionList);
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
